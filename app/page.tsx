26e4b2e0a6a029c7fb6074ef6366e178fb0fa413
Markus Strömberg <stromberg.markus@gmail.com>
2024-04-25 08:36:45 +0200

    minor visual improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SetTheme from "./components/SetTheme";
 import ToDoList from "./components/ToDoList";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
     <main>
       <div className="listHeader">
-        <h1>To do List</h1>
-        <SetTheme/>
+        <h1>
+          <span className="me-1">
+            <FontAwesomeIcon icon={faBolt} />
+          </span>
+          Just ToDo It!
+        </h1>
+        <SetTheme />
       </div>
-      <ToDoList/>
+      <ToDoList />
     </main>
   );
 }
